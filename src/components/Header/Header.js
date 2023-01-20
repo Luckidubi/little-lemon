90154ac3fe089e13b31a42adf283ae0fa565cade
Dubem <nwodochidubem1@gmail.com>
2023-01-19 22:57:16 -0500

    style: made header component responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import headerImg from "../../assets/restauranfood.jpg"
+import "./Header.css"
 function Header() {
   return (
     <>
-
+<section className="llemon__header section__padding">
+<div className='llemon__header-content'>
+  <div className='llemon__header-title'>
+<h1>Little Lemon</h1>
+<p>Chicago</p>
+  </div>
+  <div className='llemon__header-description'>
+    <p>Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+<div className='llemon__header-cta'>
+<button>Reserve a Table</button>
+</div>
+</div>
+<div className='llemon__header-image'>
+  <img src={headerImg} alt="llemon Header"/>
+</div>
+</section>
     </>
   )
 }
