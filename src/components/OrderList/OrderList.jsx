275e63fe16579c40c4adf98332adf0450aa79182
Dubem <nwodochidubem1@gmail.com>
2023-02-01 18:48:35 -0500

    chore: Order and OrderList component

---
+++ src/components/OrderList/OrderList.jsx
@@ -0,0 +1,71 @@
+import { Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import OrderCard from "../OrderCard/OrderCard";
+import "./OrderList.css";
+import useSearch from "../../hooks/useSearch";
+import { useMenuContext } from "../../context/MenuContext";
+import CartList from "../../components/CartList/CartList";
+function OrderList() {
+  const { dispatch } = useMenuContext();
+
+  const [search, setSearch] = useState("");
+  const { searchMenu } = useSearch({ search });
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  function handleAddToCart(item) {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: item,
+    });
+  }
+
+  function handleRemoveFromCart(item) {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: item,
+    });
+  }
+
+  return (
+    <>
+      <section className="llemon__OrderList section__padding">
+        <div className="llemon__OrderList-title">
+          <h1>Order Online</h1>
+        </div>
+        <div className="llemon__OrderList-search">
+          <Input
+            type="search"
+            placeholder="Search Menu"
+            value={search}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="llemon_OrderList-sidebar">
+          <main>
+            <div className="llemon__OrderList-container">
+              {searchMenu().map((item) => (
+                <OrderCard
+                  key={item.id}
+                  img={item.img}
+                  name={item.title}
+                  price={item.price}
+                  onClick1={() => handleAddToCart(item)}
+                  onClick2={() => handleRemoveFromCart(item.id)}
+                />
+              ))}
+            </div>
+          </main>
+          <aside>
+          <h2>Cart List</h2>
+            <CartList />
+          </aside>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default OrderList;
---
+++ src/pages/Order.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OrderList from '../components/OrderList/OrderList'
+
+function Order() {
+  return (
+    <>
+
+<OrderList/>
+
+
+
+    </>
+  )
+}
+
+export default Order
