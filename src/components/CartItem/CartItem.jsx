7620f0d70d7736245678d0b4d4f7fe8e05934c72
Dubem <nwodochidubem1@gmail.com>
2023-02-01 18:57:08 -0500

    feat: Add CartItem and CartList component

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./CartItem.css"
+function CartItem({title, quantity}) {
+  return (
+    <>
+    <dl className="CartItem-list">
+
+    <dt className="CartItem-list__key">{title}</dt>
+
+
+    <dd className="CartItem-list__value">X{quantity}</dd>
+</dl>
+
+    </>
+  )
+}
+
+export default CartItem
---
+++ src/components/CartList/CartList.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useMenuContext } from "../../context/MenuContext";
+import CartItem from "../CartItem/CartItem";
+import "./CartList.css";
+
+function CartList() {
+  const { cart } = useMenuContext();
+
+  const total = cart.map(item => item.price * item.quantity)
+  .reduce((acc, value) => acc + value, 0).toFixed(2);
+  return (
+    <>
+      <section className="llemon-CartList section__padding">
+        <div className="llemon-CartList__container">
+          {cart.length === 0 ? (
+            <p>Cart empty!</p>
+          ) : (
+            <>
+              {cart.map((item) => (
+                <CartItem
+                  key={item.id}
+                  title={item.title}
+                  quantity={item.quantity}
+                />
+              ))}
+              <div className="llemon-CartList__total">Total: ${total}</div>
+            </>
+          )}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default CartList;
