9fa015f46ed4964188bcaddd3c10da8842c73c6b
Dubem <nwodochidubem1@gmail.com>
2023-02-01 19:01:24 -0500

    feat: Add OrderCard component

---
+++ src/components/OrderCard/OrderCard.jsx
@@ -0,0 +1,29 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+import "./OrderCard.css"
+function OrderCard({img, name, price, onClick1, onClick2}) {
+  return (
+    <>
+    <div className="OrderCard__container">
+        <div className="OrderCard__imagebox">
+          <img src={img} alt="dish" />
+        </div>
+        <div className="OrderCard__details">
+        <div className="OrderCard__dish-price">
+          <p>{name}</p>
+          <p id="price">${price}</p>
+        </div>
+
+        <div className="OrderCard__order">
+        <Button colorScheme="yellow" onClick={onClick1}>+</Button><Button
+colorScheme="yellow"
+        onClick={onClick2}>-</Button>
+        </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default OrderCard
