7aa6cdd7627e48ef5087a0c66e9e0b543d49eb02
Dubem <nwodochidubem1@gmail.com>
2023-02-01 19:02:21 -0500

    chore: Add useSearch hook

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,18 @@
+import { useMenuContext } from "../context/MenuContext";
+
+const useSearch = ( {search} ) => {
+  const { menu } = useMenuContext();
+  const searchMenu = () => {
+    const filteredMenu = menu.filter((item) =>(
+      search === ""
+        ? item
+        : item.title.toLowerCase().includes(search.toLowerCase())
+    ));
+
+    return filteredMenu;
+  };
+
+  return {searchMenu}
+};
+
+export default useSearch;
